c253b8e2f1360dd1f9def06d41c2f15c01613e1e
github username <github email address>
2017-11-22 10:02:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import SimpleComponent from "./components/SimpleComponent";
+import SimplerComponent from "./components/SimplerComponent";
+
+function poke() {
+  alert("ouch!");
+}
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={poke} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
